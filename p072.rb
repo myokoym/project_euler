b3eb339ae1485390f4075da4d1d05cc00d5ba687
Masafumi Yokoyama <myokoym@gmail.com>
2013-07-18 12:08:15 +0900

    Update p072.rb
    completed. (about 55 sec.)

--- p072.rb
+++ p072.rb
@@ -1,21 +1,34 @@
 #!/usr/bin/env ruby
 
-def problem72(max_d)
-  1.upto(max_d).inject(0) do |sum, n|
-    sum += (n + 1).upto(max_d).count do |d|
-             Rational(n, d).numerator == n
-           end
+require "prime"
+
+def asymptotic_farey_sequence_length(n)
+  (3 * (n ** 2)) / (Math::PI ** 2)
+end
+
+def totient_function(n)
+  n.prime_division.inject(n) do |sum, factor|
+    p = factor.first
+    sum * (1 - 1.0 / p)
+  end
+end
+
+def farey_sequence_length(n)
+  2.upto(n).inject(0) do |sum, d|
+    sum + totient_function(d)
   end
 end
 
 # example
-p problem72(8)
+p [8, farey_sequence_length(8), asymptotic_farey_sequence_length(8)]
 
 # test
 require "benchmark"
 Benchmark.bm do |x|
   1.upto(6) do |i|
     max_d = 10 ** i
-    x.report { p [max_d, problem72(max_d)] }
+    answer = nil
+    x.report { answer = farey_sequence_length(max_d) }
+    p [max_d, answer, asymptotic_farey_sequence_length(max_d)]
   end
 end
