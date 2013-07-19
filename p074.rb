791f4169e7ae987cc8f92dca0f31f2110914de5c
Masafumi Yokoyama <myokoym@gmail.com>
2013-07-19 12:58:45 +0900

    Update p074.rb
    Use cache. (about 25 sec.)

--- p074.rb
+++ p074.rb
@@ -18,19 +18,40 @@ def chain_of_non_repeating_terms(n)
   terms
 end
 
+@cache = {}
+
+def number_of_non_repeating_terms(n)
+  terms = []
+  count = 0
+  term = n
+  loop do
+    if @cache.has_key?(term)
+      count += @cache[term]
+      break
+    end
+    terms << term
+    count += 1
+    term = sum_of_factorial_of_digits(term)
+    break if terms.include?(term)
+  end
+  @cache[n] = count
+  count
+end
+
 # example
 p sum_of_factorial_of_digits(145)
 p sum_of_factorial_of_digits(169)
 example = chain_of_non_repeating_terms(69)
 p example
 p example.count
+p number_of_non_repeating_terms(69)
 
 # test
 require "benchmark"
 Benchmark.bm do |x|
   x.report do
     answer = (1...1_000_000).select do |i|
-                chain_of_non_repeating_terms(i).count == 60
+                number_of_non_repeating_terms(i) == 60
               end
     p answer
     p answer.count
