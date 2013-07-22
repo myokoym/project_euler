9bb442291f4b2b4b32d027897fb7656143b1c925
Masafumi Yokoyama <myokoym@gmail.com>
2013-07-22 13:07:01 +0900

    Create p076.rb
    It cannot use because it is too slow!!

---
+++ p076.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+def counting_summations(sum)
+  2.upto(sum).flat_map do |size|
+    (1...sum).to_a.repeated_combination(size).select do |a|
+      a.inject(:+) == 5
+    end
+  end
+end
+
+# example
+p counting_summations(5)
+p counting_summations(5).count
+
+# test
+require "benchmark"
+Benchmark.bm do |x|
+  10.step(100, 1) do |sum|
+    x.report { p [sum, counting_summations(sum).count] }
+  end
+end
